2e04543111fcc2ffec2b597192a0a1631d326c88
Clint Worrell <github@clintworrell.com>
2017-04-05 14:09:33 -0700

    Get Express to dynamically run on either Heroku prod port or local dev port 3000

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-const PORT = process.env["PORT"];
+const PORT = process.env["PORT"] ? process.env["PORT"] : 3000;
 
 app.use(express.static('./'));
 
